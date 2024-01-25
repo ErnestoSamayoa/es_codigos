896f9e6ff657504231a524f936ce9df967815f32
ErnestoSamayoa <esamayoaj1@miumg.edu.gt>
2024-01-24 20:26:27 -0600

    Updated

---
+++ Ej_1.py
@@ -0,0 +1,13 @@
+## Programa que calcule grados celsius a Fahrenheit
+
+print("*Bienvenido usuario*")
+
+celsius = float(input("Ingresa la temperatura en grados Celsius: "))
+
+## Convertir a Fahrenheit y mostrar el resultado
+fahrenheit = (celsius * 9/5) + 32
+
+print(f"{celsius} grados Celsius son equivalen a {fahrenheit} grados Fahrenheit.")
+
+
+print("*********************")
